85fa5098495adc7dbd912c1ed2ebe534bdb3cb7a
Sam Bernheim <sambernheim@gmail.com>
2016-07-18 14:05:05 -0400

    only .bundle.min.js.map file now needs to be in proj dir + added basic express server

---
+++ server.js
@@ -0,0 +1,9 @@
+'use strict'
+var express = require('express');
+var app = express();
+
+app.use('/', express.static('./'));
+
+app.listen(3000, function () {
+  console.log('example app listening on port 3000!');
+});
